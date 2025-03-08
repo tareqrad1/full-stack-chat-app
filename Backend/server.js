1703ad25fb40700b9790b901fb026ed73fd4f835
Tareq Radi <raditareq15@gmail.com>
2025-03-08 13:40:26 +0200

    v1- end backend without socket

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import { connectDb } from './config/connectDb.config.js';
 import cookieParser from 'cookie-parser';
 import messageRoute from './routers/message.route.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -14,6 +15,11 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials: true,
+}));
+
 //routes
 app.use('/api/auth', authRoute);
 app.use('/api/message', messageRoute);
